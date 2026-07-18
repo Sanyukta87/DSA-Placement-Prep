20b2d6211dae7fc43f33bb809302ec91c9a1c8b2
Sanyukta87 <sanyuktadhume@gmail.com>
2026-07-18 16:21:33 +0530

    Time: 0 ms (100.00%) | Memory: 9.2 MB (68.63%) - LeetSync

---
+++ 409-longest-palindrome/longest-palindrome.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    int longestPalindrome(string s) {
+
+        int freq[128] = {0};   
+        
+        for(int i = 0; i < s.length(); i++)
+{
+    freq[s[i]]++;
+}
+
+        int ans = 0;
+        bool odd = false;
+
+     for(int i = 0; i < 128; i++)
+{
+    if(freq[i] % 2 == 0)
+    {
+        ans += freq[i];
+    }
+    else
+    {
+        ans += freq[i] - 1;
+        odd = true;
+    }
+}
+    
+if(odd)
+    ans++;
+    return ans;
+    }
+};
