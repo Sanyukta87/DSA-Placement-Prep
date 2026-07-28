7cce78081365a1c1b50dbe420adfba49fa7ca600
Sanyukta87 <sanyuktadhume@gmail.com>
2026-07-28 19:51:24 +0530

    Time: 632 ms (29.03%) | Memory: 295.6 MB (32.40%) - LeetSync

---
+++ 948-sort-an-array/sort-an-array.cpp
@@ -0,0 +1,66 @@
+void merge(vector<int>& arr, int low, int mid, int high)
+{
+    vector<int> temp;
+
+    int left = low;
+    int right = mid + 1;
+
+    // Compare elements from both halves
+    while(left <= mid && right <= high)
+    {
+        if(arr[left] <= arr[right])
+        {
+            temp.push_back(arr[left]);
+            left++;
+        }
+        else
+        {
+            temp.push_back(arr[right]);
+            right++;
+        }
+    }
+
+    // Copy remaining elements from left half
+    while(left <= mid)
+    {
+        temp.push_back(arr[left]);
+        left++;
+    }
+
+    // Copy remaining elements from right half
+    while(right <= high)
+    {
+        temp.push_back(arr[right]);
+        right++;
+    }
+
+    // Copy sorted elements back to original array
+    for(int i = low; i <= high; i++)
+    {
+        arr[i] = temp[i - low];
+    }
+}
+
+void mergeSort(vector<int>& arr, int low, int high)
+{
+    if(low >= high)
+        return;
+
+    int mid = low + (high - low) / 2;
+
+    mergeSort(arr, low, mid);
+
+    mergeSort(arr, mid + 1, high);
+
+    merge(arr, low, mid, high);
+}
+class Solution {
+public:
+
+vector<int> sortArray(vector<int>& nums) {
+
+        mergeSort(nums, 0, nums.size() - 1);
+
+        return nums;
+    }
+};
