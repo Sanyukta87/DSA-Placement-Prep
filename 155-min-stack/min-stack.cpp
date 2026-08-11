a188110e38cd1d3981356097bf6d09f8efc70747
Sanyukta87 <sanyuktadhume@gmail.com>
2026-08-11 19:04:59 +0530

    Time: 56 ms (64.11%) | Memory: 151.4 MB (27.26%) - LeetSync

---
+++ 155-min-stack/min-stack.cpp
@@ -0,0 +1,46 @@
+class MinStack {
+public:
+
+    stack<int> st;       // Stores all values
+    stack<int> minSt;    // Stores minimum values
+
+    MinStack() {
+        // Constructor
+    }
+
+    void push(int x) {
+
+        st.push(x);      // Always push into normal stack
+
+        // Push into min stack only if
+        // x becomes a new minimum
+        if(minSt.empty() || x <= minSt.top())
+        {
+            minSt.push(x);
+        }
+    }
+
+    void pop() {
+
+        // If removing current minimum,
+        // remove it from min stack too
+        if(st.top() == minSt.top())
+        {
+            minSt.pop();
+        }
+
+        st.pop();        // Remove from normal stack
+    }
+
+    int top() {
+
+        // Return current top
+        return st.top();
+    }
+
+    int getMin() {
+
+        // Minimum is always at top of min stack
+        return minSt.top();
+    }
+};
